d6be8c43408e289e5b36a57a2c2c101297d39b52
Félix Piédallu <felix@piedallu.me>
2019-03-15 18:59:49 +0100

    adc ok ?

--- lowlevel/adc.c
+++ lowlevel/adc.c
@@ -5,74 +5,56 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/adc.h>
 #include <libopencm3/stm32/dma.h>
+#include <libopencm3/cm3/nvic.h>
 
-// ACS711KLCTR-25AB-T
-// (55mv/A)
-
-volatile uint16_t adc_res[2];
 
 void adcs_setup() {
   rcc_periph_clock_enable(RCC_GPIOB);
   rcc_periph_clock_enable(RCC_ADC);
-  rcc_periph_clock_enable(RCC_DMA1);
 
   gpio_mode_setup(GPIOB, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO0);
   gpio_mode_setup(GPIOB, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO1);
-
-  // DMA Setup
-
-  dma_disable_channel(DMA1, DMA_CHANNEL1);
-
-  dma_enable_circular_mode(DMA1, DMA_CHANNEL1);
-  dma_enable_memory_increment_mode(DMA1, DMA_CHANNEL1);
-
-  dma_set_peripheral_size(DMA1, DMA_CHANNEL1, DMA_CCR_PSIZE_16BIT);
-  dma_set_memory_size(DMA1, DMA_CHANNEL1, DMA_CCR_MSIZE_16BIT);
-
-  dma_set_read_from_peripheral(DMA1, DMA_CHANNEL1);
-  dma_set_peripheral_address(DMA1, DMA_CHANNEL1, ADC_DR(ADC1));
-
-  dma_set_memory_address(DMA1, DMA_CHANNEL1, (uint32_t) &adc_res);
-  dma_set_number_of_data(DMA1, DMA_CHANNEL1, 2);
-
-  dma_enable_transfer_complete_interrupt(DMA1, DMA_CHANNEL1);
-  dma_enable_channel(DMA1, DMA_CHANNEL1);
 
 
   // ADC Setup
   adc_power_off(ADC1);
   adc_set_continuous_conversion_mode(ADC1);
-  adc_disable_discontinuous_mode_regular(ADC1);
-
-  // adc_enable_external_trigger_regular(ADC1, )
-  adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_7DOT5CYC);
   adc_set_right_aligned(ADC1);
+
+  adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_61DOT5CYC);
+  adc_set_resolution(ADC1, ADC_CFGR1_RES_12_BIT);
+
+  uint8_t channel_seq[16] = { 1, 2 };
+  adc_set_regular_sequence(ADC1, 2, channel_seq);
 
   adc_power_on(ADC1);
 
   delay_ms(1);
-
   adc_calibrate(ADC1);
-
-  // ADC1_IN1,2 (PB0, PB1)
-  uint8_t channel_seq[] = { 1, 2 };
-  adc_set_regular_sequence(ADC1, 1, channel_seq);
-  adc_set_resolution(ADC1, ADC_CFGR1_RES_12_BIT);
-
   delay_ms(1);
 }
 
+uint32_t adc_a, adc_b;
+void adc_read_all() {
+  adc_a = adc_read_regular(ADC1);
+  adc_b = adc_read_regular(ADC1);
+}
+
 uint32_t adc_a_value() {
-  adc_start_conversion_regular(ADC1);
-  while (!(adc_eoc(ADC1)));
-  // temp=adc_read_regular(ADC1);
-  // gpio_port_write(GPIOE, temp << 4);
-  // my_usart_print_int(USART2, temp);
-
-
-  return 0;
+  adc_read_all();
+  return adc_a;
 }
 
 uint32_t adc_b_value() {
-  return 0;
+  adc_read_all();
+  return adc_b;
+}
+
+double convert_adc_to_milliamps(uint32_t adc_value) {
+  // ACS711KLCTR-25AB-T : 0.055V/A
+  const double acs_ma_by_mv = 1/0.055;
+  const double mv_by_unit = 3300.0 / 4096; // 8.056mV
+  double mv = 1.0 * adc_value * mv_by_unit;
+  double ma = mv * acs_ma_by_mv;
+  return ma;
 }
