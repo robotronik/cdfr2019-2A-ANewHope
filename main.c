ea69602fb4ad3e61f4997278f2ab6b24234ff7c8
Félix Piédallu <felix@piedallu.me>
2019-04-13 18:18:58 +0200

    Test moteurs

--- main.c
+++ main.c
@@ -9,15 +9,24 @@ int main() {
   eeprom_setup();
   encoders_setup();
 
+  motors_setup();
+
   // set_all_1();
   bool status = about_da_power();
 
 
+  while(true) {
+    motor_b_set(0);
+    delay_ms(10);
+    motor_b_set(1);
+    delay_ms(1);
+  }
 
-  led_set_status(1);
-  delay_ms(1000);
-  led_set_status(0);
-  delay_ms(100);
+
+    led_set_status(1);
+    delay_ms(1000);
+    led_set_status(0);
+    delay_ms(100);
 
 
   while(true) {
